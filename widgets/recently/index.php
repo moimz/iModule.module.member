eb7adf0d7cace1f2d03d5ed17b15068531338223
arzzcom <arzz@arzz.com>
2018-08-27 11:28:35 +0900

    최근회원목록 위젯 수정

---
+++ widgets/recently/index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * 이 파일은 iModule 회원모듈의 일부입니다. (https://www.imodule.kr)
+ *
+ * 최근 가입한 회원목록을 가져온다.
+ * 
+ * @file /modules/member/widgets/recently/index.php
+ * @author Arzz (arzz@arzz.com)
+ * @license MIT License
+ * @version 3.0.0
+ * @modified 2018. 8. 26.
+ */
+if (defined('__IM__') == false) exit;
+
+$count = $Widget->getValue('count');
+$cache = $Widget->getValue('cache');
+
+if ($Widget->checkCache() < time() - $cache) {
+	$lists = $me->db()->select($me->getTable('member'))->where('status','ACTIVATED');
+	$lists = $lists->limit($count)->orderBy('reg_date','desc')->get('idx');
+	for ($i=0, $loop=count($lists);$i<$loop;$i++) {
+		$lists[$i] = $me->getMember($lists[$i]);
+	}
+	
+	$Widget->storeCache(json_encode($lists,JSON_UNESCAPED_UNICODE));
+} else {
+	$lists = json_decode($Widget->getCache());
+}
+
+return $Templet->getContext('index',get_defined_vars());
+?>
