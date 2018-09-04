be68e5e777df342be69c449f2d789254c74b9132
arzzcom <arzz@arzz.com>
2018-09-04 15:57:41 +0900

    포인트 내역 템플릿 수정

--- templets/default/point.php
+++ templets/default/point.php
@@ -1,4 +1,19 @@
-<div data-role="tool">
+<?php
+/**
+ * 이 파일은 iModule 회원모듈의 일부입니다. (https://www.imodule.kr)
+ *
+ * 포인트 내역 템플릿
+ * 
+ * @file /modules/moimz/templets/default/point.php
+ * @author Arzz (arzz@arzz.com)
+ * @license MIT License
+ * @version 3.0.0
+ * @modified 2018. 9. 3.
+ */
+
+$IM->loadWebFont('Roboto');
+?>
+<div data-role="toolbar">
 	<h4>총 포인트 : <?php echo number_format($member->point); ?></h4>
 	
 	<div data-role="input">
@@ -9,11 +24,12 @@
 		</select>
 	</div>
 </div>
-<ul data-role="table" class="black inner point">
+
+<ul data-role="table" class="black inner">
 	<li class="thead">
 		<span class="loopnum">번호</span>
 		<span class="title">내역</span>
-		<span class="count">변동</span>
+		<span class="change">변동</span>
 		<span class="accumulation">누적포인트</span>
 		<span class="date">날짜 <i class="fa fa-caret-down"></i></span>
 	</li>
@@ -21,7 +37,7 @@
 	<li class="tbody">
 		<span class="loopnum"><?php echo $item->loopnum; ?></span>
 		<span class="title"><?php echo $item->content; ?></span>
-		<span class="count"><?php echo $item->point > 0 ? '<span class="increase">+'.number_format($item->point).'</span>' : '<span class="decrease">'.number_format($item->point).'</span>'; ?></span>
+		<span class="change"><?php echo $item->point > 0 ? '<span class="increase">+'.number_format($item->point).'</span>' : '<span class="decrease">'.number_format($item->point).'</span>'; ?></span>
 		<span class="accumulation"><?php echo $item->accumulation > 0 ? '<span class="increase">+'.number_format($item->accumulation).'</span>' : '<span class="decrease">'.number_format($item->accumulation).'</span>'; ?></span>
 		<span class="date"><?php echo GetTime('Y-m-d(D) H:i',$item->reg_date); ?></span>
 	</li>
@@ -34,37 +50,3 @@
 </ul>
 
 <div class="pagination"><?php echo $pagination; ?></div>
-
-<?php /*
-<section class="row">
-	<aside>
-		
-	</aside>
-	
-	<section data-role="grid">
-		<div class="gridBox">
-			<table>
-				<thead>
-					<tr><?php /*
-						<td style="width:60%;"><?php echo $Module->getLanguage('point/content'); ?></td>
-						<td style="width:80px;"><?php echo $Module->getLanguage('point/point'); ?></td>
-						<td style="width:100px;"><?php echo $Module->getLanguage('point/reg_date'); ?></td>
-						 ?>
-					</tr>
-				</thead>
-				<tbody>
-					<?php for ($i=0, $loop=count($lists);$i<$loop;$i++) { ?>
-					<tr>
-						<td><?php echo $lists[$i]->content; ?></td>
-						<td class="number fontBold <?php echo $lists[$i]->point > 0 ? 'fontBlue' : 'fontRed'; ?> right"><?php echo $lists[$i]->point > 0 ? '+' : ''; ?><?php echo number_format($lists[$i]->point); ?></td>
-						<td class="number"><?php echo GetTime('Y.m.d H:i',$lists[$i]->reg_date); ?></td>
-					</tr>
-					<?php } ?>
-				</tbody>
-			</table>
-		</div>
-		
-		<div class="center"><?php echo $pagination->html; ?></div>
-	</section>
-</section>
-*/ ?>
