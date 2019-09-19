7e8aeabf5e80b4f9d64108ce48e9aa2125fbf59c
arzzcom <arzz@arzz.com>
2019-09-19 11:47:00 +0900

    메뉴타이틀 출력방식 변경

--- admin/index.php
+++ admin/index.php
@@ -213,7 +213,7 @@ Ext.onReady(function () { Ext.getCmp("iModuleAdminPanel").add(
 					itemcontextmenu:function(grid,record,item,index,e) {
 						var menu = new Ext.menu.Menu();
 						
-						menu.add('<div class="x-menu-title">'+record.data.name+'</div>');
+						menu.addTitle(record.data.name);
 						
 						menu.add({
 							iconCls:"xi xi-form",
@@ -402,7 +402,7 @@ Ext.onReady(function () { Ext.getCmp("iModuleAdminPanel").add(
 									itemcontextmenu:function(grid,record,item,index,e) {
 										var menu = new Ext.menu.Menu();
 										
-										menu.add('<div class="x-menu-title">'+record.data.title+'</div>');
+										menu.addTitle(record.data.title);
 										
 										menu.add({
 											iconCls:"xi xi-form",
@@ -554,7 +554,7 @@ Ext.onReady(function () { Ext.getCmp("iModuleAdminPanel").add(
 							itemcontextmenu:function(grid,record,item,index,e) {
 								var menu = new Ext.menu.Menu();
 								
-								menu.add('<div class="x-menu-title">'+record.data.title+'</div>');
+								menu.addTitle(record.data.title);
 								
 								menu.add({
 									iconCls:"xi xi-form",
